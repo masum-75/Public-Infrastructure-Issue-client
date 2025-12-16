aa0ee3828eba63524b0cf589ba89c3006f8ce33d
masum-75 <tanbirmasum014@gmail.com>
2025-12-16 21:06:20 +0600

    private route setup

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import useAuth from '../hooks/useAuth';
+import { Navigate, useLocation } from 'react-router';
+import Lottie from 'lottie-react';
 
-const PrivateRoutes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    // animationData: loaderData,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+};
+
+const PrivateRoutes = ({ children }) => {
+    const { user, loading } = useAuth();
+    const location = useLocation();
+
+    if (loading) {
+        return (
+            <div className='flex items-center justify-center min-h-screen'>
+                 <Lottie options={defaultOptions} height={300} width={300} />
+            </div>
+        );
+    }
+
+    if (user) {
+        return children;
+    }
+
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
 export default PrivateRoutes;
